5de7d7e333e327a34b35aa262281edbef4580451
Nishar24 <mdnishar75640@gmail.com>
2025-07-13 11:51:20 +0530

    push variable.js file

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144622
+let accountEmail = "nishar@google.com"
+var accountPassword = "1426337"
+accountCity = "Delhi"
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+accountEmail = "nishu@gmail.com"
+//noaccountPassword = "jhj543"
+//accountCity = "Patna"
+console.table([ accountEmail, accountPassword, accountCity]);
+
