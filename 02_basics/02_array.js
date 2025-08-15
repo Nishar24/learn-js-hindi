ac98f24625ff61c953ff572153eef9fca7592c34
Nishar24 <mdnishar75640@gmail.com>
2025-08-15 22:24:38 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -13,6 +13,7 @@ const all_new_heros = [...marvel_heros,...dc_heros]
 
 //console.log(all_new_heros);
 
+
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]//merge all arrays in one array
 
 const real_another_array = another_array.flat(Infinity)
