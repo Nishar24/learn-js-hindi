b992dbba4747be35d358f9a93043adfdeafb2abb
Nishar24 <mdnishar75640@gmail.com>
2025-09-22 08:38:58 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -6,7 +6,6 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
-
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
