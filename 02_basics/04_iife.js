3599a2068d532c2ffceea4d390d52d04a918dd34
Nishar <mdnishar75640@gmail.com>
2026-01-28 23:51:06 +0530

    Add named and anonymous IIFE examples

--- 02_basics/04_iife.js
+++ 02_basics/04_iife.js
@@ -1,13 +1,17 @@
 //Immediately Invoked Function Expressions (IIFE)
 
-
 (function chai (){
     //named IIFE
     console.log(`DB CONNECTED`);
 })(); 
+
 
 //() () //1st paranthesis for definition of function & 2nd for execution
 
 ((name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 })('Nishar')
+
+
+
+
