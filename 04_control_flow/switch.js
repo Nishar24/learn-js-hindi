90614974104701cb7d8a5b696f9561ddac7e7578
Nishar <mdnishar75640@gmail.com>
2026-01-29 22:14:00 +0530

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// switch () {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    
+    case 2:
+        console.log("february");
+        break;
+
+    case 3:
+        console.log("march");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
