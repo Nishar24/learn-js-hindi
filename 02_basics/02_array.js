f9b66496277d2cbf7b35da6a12c479871275487c
Nishar <mdnishar75640@gmail.com>
2026-02-13 22:43:30 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,4 +1,4 @@
-const marvel_heros = ["thor", "Ironman", "spiderman"]
+const marvel_heros = ["Nishar", "Manzar", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
 //marvel_heros.push(dc_heros)
