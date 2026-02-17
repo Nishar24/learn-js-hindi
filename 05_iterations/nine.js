baf173e83fbb8e32998da864e40a541730b033b5
Nishar <mdnishar75640@gmail.com>
2026-02-17 22:54:25 +0530

    Update nine.js

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -1,7 +1,7 @@
 const myNums = [1, 2, 3]
 
 const myTotal = myNums.reduce(function (acc, currval) {
-//     console.log(`acc: ${acc} and currval: ${currval}`);
+     console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
 // }, 0)
 
@@ -33,3 +33,4 @@ const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0);
 
 
 console.log(priceToPay);
+
