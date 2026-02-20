4ba2d193384fa7b90485651504dbbca978f354c8
Nishar <mdnishar75640@gmail.com>
2026-02-20 23:02:03 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -15,7 +15,7 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email);
+ console.log(JsUser.email);
 // console.log(JsUser["email"]);
 // console.log(JsUser["full name"]);
 // console.log(JsUser[mySym]);
