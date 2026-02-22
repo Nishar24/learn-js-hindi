5fce70c77a0fa39650277c646ba7c2cd238ea311
Nishar <mdnishar75640@gmail.com>
2026-02-22 22:44:26 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -16,7 +16,7 @@ const JsUser = {
 }
 
  console.log(JsUser.email);
-// console.log(JsUser["email"]);
+ console.log(JsUser["email"]);
 // console.log(JsUser["full name"]);
 // console.log(JsUser[mySym]);
 
