9f337cc9187a4e51d804195df0edad93a0ac295c
Nishar <mdnishar75640@gmail.com>
2026-09-10 23:02:11 +0530

    Refactor functions and update addTwoNumbers call

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,7 +22,7 @@ function addTwoNumbers(number1, number2){
     return number1 + number2
 }
 
-const result = addTwoNumbers(3, 4)
+const result = addTwoNumbers(4, 4)
 
 //console.log("Result: ", result);
 
